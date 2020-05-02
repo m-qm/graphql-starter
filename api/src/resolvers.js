d4609a1aa09938234beb79c7a0803cc177e2884f
m-qm <mireiaquerol@gmail.com>
2020-05-02 23:47:59 +0200

    add queries to model

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,17 @@
 
 module.exports = {
   Query: {
-    
+    pets (_, __, ctx) {
+      return ctx.models.Pet.findMany ({});
+    },
   },
-  Mutation: {
-    
-  },
+  //Mutation: {},
   Pet: {
-    img(pet) {
+    img (pet) {
       return pet.type === 'DOG'
         ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
+        : 'http://placekitten.com/300/300';
+    },
   },
-  User: {
-    
-  }
-}
+  //User: {},
+};
